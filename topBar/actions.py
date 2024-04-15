7e96235e6c1cbad745d812cc1c7f8e2341f41c3d
savin-lapeze <114133845+savin-lapeze@users.noreply.github.com>
2024-04-15 15:21:09 +0200

    ouverture pdf
    ouverture pdf

--- topBar/actions.py
+++ topBar/actions.py
@@ -1,23 +1,13 @@
 import pandas as pd
 from tkinter import filedialog
 import os
-
-def helptest():
-    filename = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF files", "*.pdf")])
-  
+import subprocess
 
 def help():
-    # Spécifiez le chemin du fichier PDF
-    pdf_file_path = "Tuto_quiz_moodle_v1.pdf"
+    pdf_file_path = "help.pdf"
 
-    # Demandez à l'utilisateur où enregistrer le fichier
-    destination_file = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF files", "*.pdf")])
-
-    # Copier le fichier PDF dans le répertoire de destination
-    if destination_file:
-        # Vérifiez si le fichier PDF existe
-        if os.path.exists(pdf_file_path):
-            # Copiez le fichier PDF dans le répertoire de destination
-            with open(pdf_file_path, "rb") as f_read:
-                with open(destination_file, "wb") as f_write:
-                    f_write.write(f_read.read())
+    # Vérifie si le fichier PDF existe
+    os.path.exists(pdf_file_path)
+        # Ouvre le fichier PDF avec le programme associé
+    os.startfile(pdf_file_path)
+    
